757c64f9854f47219f4a6009c6d5769b87ebf996
Funtijn <hemeryckmattias@hotmail.com>
2021-12-03 18:56:57 +0100

    Create app.js

---
+++ Scripts/app.js
@@ -0,0 +1,92 @@
+let API = null;
+
+window.onload = async function () {
+    API = await Workspace.connect(window.parent, (event, data) => {
+        //console.log("Event: ", event, data);
+        var eventName = event.split(".").pop();
+
+        switch (eventName) {
+            case "onPicked":
+                OnPicked(data.data);
+                break;
+        }
+
+    });
+}
+
+// Method to refresh poins and to set when to start watching
+function OnPickedRefresh() {
+    origin = undefined;
+    point1 = undefined;
+    point2 = undefined;
+    canCalculate = true;
+    var txtAngle = document.getElementById('txtAngle');
+    txtAngle.value = "Pick center point";
+}
+
+// Determines whether watch picked points
+let canCalculate = false;
+
+// First picked point
+let origin;
+
+// Second picked points
+let point1;
+
+// Last picked point
+let point2;
+
+// OnPicked event called every time picker picks point
+function OnPicked(pickedData) {
+    if (!canCalculate) {
+        return;
+    }
+
+    var txtAngle = document.getElementById('txtAngle');
+
+    if (origin == undefined) {
+        origin = pickedData.position;
+        txtAngle.value = "Pick second point";
+        //console.log("P1: " + origin);
+    } else if (point1 == undefined) {
+        point1 = pickedData.position;
+        txtAngle.value = "Pick third point";
+        //console.log("P2: " + point1);
+    } else if (point2 == undefined) {
+        point2 = pickedData.position;
+        //console.log("P3: " + point2);
+    }
+
+    if (origin != undefined && point1 != undefined && point2 != undefined) {
+        var vector1 = GetVectorLineSegment(origin, point1);
+        var vector2 = GetVectorLineSegment(origin, point2);
+
+        var angle = Math.acos(
+            (vector1.X * vector2.X + vector1.Y * vector2.Y + vector1.Z * vector2.Z) /
+            Math.sqrt((vector1.X * vector1.X + vector1.Y * vector1.Y + vector1.Z * vector2.Z) * (vector2.X * vector2.X + vector2.Y * vector2.Y + vector2.Z * vector2.Z))
+        );
+
+        var txtAngle = document.getElementById('txtAngle');
+        txtAngle.value = +ToDegree(angle).toFixed(3);
+        canCalculate = false;
+    }
+}
+
+// Gets vector from two points
+function GetVectorLineSegment(point1, point2) {
+    return new vector(point1.x - point2.x, point1.y - point2.y, point1.z - point2.z);
+}
+
+// Recalculate Radians to degrees
+function ToDegree(angle) {
+    return angle * (180.0 / Math.PI);
+}
+
+// My Vector class
+class vector {
+    constructor(x, y, z) {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
+}
